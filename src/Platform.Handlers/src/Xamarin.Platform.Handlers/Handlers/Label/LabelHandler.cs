00d987747782374945173f7680b894355b0b6503
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-18 12:26:39 +0100

    Removed not implemented Label properties

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
@@ -5,19 +5,7 @@
 		public static PropertyMapper<ILabel, LabelHandler> LabelMapper = new PropertyMapper<ILabel, LabelHandler>(ViewHandler.ViewMapper)
 		{
 			[nameof(IText.Text)] = MapText,
-			[nameof(IText.TextColor)] = MapTextColor,
-			[nameof(IText.Font)] = MapFont,
-			[nameof(IText.TextTransform)] = MapTextTransform,
-			[nameof(IText.CharacterSpacing)] = MapCharacterSpacing,
-			[nameof(ILabel.LineHeight)] = MapLineHeight,
-			[nameof(ILabel.FontSize)] = MapFontSize,
-			[nameof(ILabel.FontAttributes)] = MapFontAttributes,
-			[nameof(ILabel.HorizontalTextAlignment)] = MapHorizontalTextAlignment,
-			[nameof(ILabel.VerticalTextAlignment)] = MapVerticalTextAlignment,
-			[nameof(ILabel.TextDecorations)] = MapTextDecorations,
-			[nameof(ILabel.LineBreakMode)] = MapLineBreakMode,
-			[nameof(ILabel.MaxLines)] = MapMaxLines,
-			[nameof(IPadding.Padding)] = MapPadding
+			[nameof(IText.TextColor)] = MapTextColor
 		};
 
 		public LabelHandler() : base(LabelMapper)
